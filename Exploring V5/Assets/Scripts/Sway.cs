1b48fa704bfbd2ee34adb1ce4eb86690e765f13d
DjAgha <omariozack@gmail.com>
2020-09-14 19:11:59 +0200

    Gun Sway

---
+++ Exploring V5/Assets/Scripts/Sway.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sway : MonoBehaviour
+{
+    #region Variables
+
+    public float intensity;
+    public float smooth;
+    private Quaternion _originRotation;
+    #endregion
+
+    #region MonoBehaviour Callbacks
+
+    private void Start()
+    {
+        _originRotation = transform.localRotation;   
+    }
+
+    void Update()
+    {
+        UpdateSway();
+    }
+    #endregion
+
+    void UpdateSway()
+    {
+        // Controls
+        float xMouse = Input.GetAxis("Mouse X");
+        float yMouse = Input.GetAxis("Mouse Y");
+
+        //Calculate target rotation
+        Quaternion xAdj = Quaternion.AngleAxis(-intensity * xMouse, Vector3.up);
+        Quaternion yAdj = Quaternion.AngleAxis(intensity * yMouse, Vector3.right);
+        Quaternion _targetRotation = _originRotation * xAdj * yAdj;
+
+        // Rotate towards target
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, _targetRotation, Time.deltaTime * smooth);
+    }
+}
