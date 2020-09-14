cd876d91b06909c91a739a5f3ee2be73101cc5c5
DjAgha <omariozack@gmail.com>
2020-09-14 03:32:33 +0200

    Num 1 for Primary Pistol

--- Exploring V5/Assets/Scripts/Weapon.cs
+++ Exploring V5/Assets/Scripts/Weapon.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
+    public GameObject[] loadout;
+    public Transform weaponParent;
+    private GameObject _currentWeapon;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,16 @@ public class Weapon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Alpha1)) Equip(0);
+    }
+
+    void Equip(int id)
+    {
+        if (_currentWeapon != null) Destroy(_currentWeapon);
+
+        GameObject newGun = Instantiate(loadout[id], weaponParent.position, weaponParent.rotation, weaponParent) as GameObject;
+        newGun.transform.localPosition = Vector3.zero;
+        newGun.transform.localEulerAngles = Vector3.zero;
+        _currentWeapon = newGun;
     }
 }
