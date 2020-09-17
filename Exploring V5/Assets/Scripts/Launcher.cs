48252a24d660ed25a954aaadc760d9d1ea531296
DjAgha <omariozack@gmail.com>
2020-09-17 21:02:48 +0200

    New Scene and Finished Setup

---
+++ Exploring V5/Assets/Scripts/Launcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class Launcher : MonoBehaviour
+{
+    public void OnEnable()
+    {
+        PhotonNetwork.AutomaticallySyncScene = true;
+        Connect();
+    }
+
+    public void Connect()
+    {
+
+    }
+
+    public void Join()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+    }
+}
