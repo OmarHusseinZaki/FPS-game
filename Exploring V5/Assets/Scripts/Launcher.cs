64d4873716372a39f89d686a5fcef5dfcc9b3ec9
DjAgha <omariozack@gmail.com>
2020-09-17 22:02:18 +0200

    Loading a Scene
    However nothing still is sent over to the server

--- Exploring V5/Assets/Scripts/Launcher.cs
+++ Exploring V5/Assets/Scripts/Launcher.cs
@@ -3,26 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 
-public class Launcher : MonoBehaviour
+public class Launcher : MonoBehaviourPunCallbacks
 {
-    public void OnEnable()
+    public void Awake()
     {
         PhotonNetwork.AutomaticallySyncScene = true;
         Connect();
     }
 
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected");
+
+        Join();
+
+        base.OnConnectedToMaster();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        StartGame();
+
+        base.OnJoinedRoom();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Create();
+
+        base.OnJoinRandomFailed(returnCode, message);
+    }
+
     public void Connect()
     {
-
+        Debug.Log("Trying To Connect...");
+        PhotonNetwork.GameVersion = "0.0.0";
+        PhotonNetwork.ConnectUsingSettings();
     }
 
     public void Join()
     {
+        PhotonNetwork.JoinRandomRoom();
+    }
 
+    public void Create()
+    {
+        PhotonNetwork.CreateRoom("");
     }
 
     public void StartGame()
     {
-
+        if(PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            PhotonNetwork.LoadLevel(1);
+        }
     }
 }
