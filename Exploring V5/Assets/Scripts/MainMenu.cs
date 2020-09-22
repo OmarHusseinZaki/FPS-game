b9965207be71bf920a18ba535d8597948550d262
DjAgha <omariozack@gmail.com>
2020-09-23 01:09:56 +0200

    Main Menu Setup and Buttons
    Button audio using Bfxr

---
+++ Exploring V5/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public Launcher launcher;
+
+    public void JoinMatch()
+    {
+        launcher.Join();
+    }
+
+    public void CreateMatch()
+    {
+        launcher.Create();
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
