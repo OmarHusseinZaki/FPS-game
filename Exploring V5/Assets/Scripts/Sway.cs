a8a285234d8b16debd962f048b439854cb06c070
DjAgha <omariozack@gmail.com>
2020-09-26 20:58:39 +0200

    Fixed Sway
    another bug, when player respawns, ammo refills for all players

--- Exploring V5/Assets/Scripts/Sway.cs
+++ Exploring V5/Assets/Scripts/Sway.cs
@@ -4,49 +4,58 @@ using UnityEngine;
 using Photon.Pun;
 
 
-public class Sway : MonoBehaviourPunCallbacks
+public class Sway : MonoBehaviour
 {
     #region Variables
 
     public float intensity;
     public float smooth;
     public bool isMine;
-    private Quaternion _originRotation;
+
+    private Quaternion origin_rotation;
+
     #endregion
+
+
 
     #region MonoBehaviour Callbacks
 
     private void Start()
     {
-        _originRotation = transform.localRotation;   
+        origin_rotation = transform.localRotation;
     }
 
-    void Update()
+    private void Update()
     {
-        if (!photonView.IsMine) return;
-
         UpdateSway();
     }
+
     #endregion
 
-    void UpdateSway()
+
+
+    #region Private Methods
+
+    private void UpdateSway()
     {
-        // Controls
-        float xMouse = Input.GetAxis("Mouse X");
-        float yMouse = Input.GetAxis("Mouse Y");
+        //controls
+        float t_x_mouse = Input.GetAxis("Mouse X");
+        float t_y_mouse = Input.GetAxis("Mouse Y");
 
         if (!isMine)
         {
-            xMouse = 0;
-            yMouse = 0;
+            t_x_mouse = 0;
+            t_y_mouse = 0;
         }
 
-        //Calculate target rotation
-        Quaternion xAdj = Quaternion.AngleAxis(-intensity * xMouse, Vector3.up);
-        Quaternion yAdj = Quaternion.AngleAxis(intensity * yMouse, Vector3.right);
-        Quaternion _targetRotation = _originRotation * xAdj * yAdj;
+        //calculate target rotation
+        Quaternion t_x_adj = Quaternion.AngleAxis(-intensity * t_x_mouse, Vector3.up);
+        Quaternion t_y_adj = Quaternion.AngleAxis(intensity * t_y_mouse, Vector3.right);
+        Quaternion target_rotation = origin_rotation * t_x_adj * t_y_adj;
 
-        // Rotate towards target
-        transform.localRotation = Quaternion.Lerp(transform.localRotation, _targetRotation, Time.deltaTime * smooth);
+        //rotate towards target rotation
+        transform.localRotation = Quaternion.Lerp(transform.localRotation, target_rotation, Time.deltaTime * smooth);
     }
+
+    #endregion
 }
